949217c21b732ada14d2a7c9f7feaa819384d255
MajceZmajce <marija.jovanovic@badin.rs>
2023-01-24 12:35:47 +0100

    Added new print

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ import javax.xml.transform.stream.StreamSource;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello again with new changes");
     }
 }
